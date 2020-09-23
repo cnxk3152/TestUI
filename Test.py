3959ccc704b70ac741d48613927a7454096029e4
cnxk3152 <cnxk_3152@163.com>
2020-09-23 13:46:08 +0800

    SampleTest
    login steam UI automation

---
+++ Test.py
@@ -0,0 +1,16 @@
+# -*- encoding=utf8 -*-
+__author__ = "Administrator"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+touch(Template(r"tpl1600781587015.png", record_pos=(0.238, -0.13), resolution=(1920, 1080)))
+
+text("cn76380931")
+
+sleep(1.0)
+
+touch(Template(r"tpl1600839715650.png", record_pos=(-0.031, 0.007), resolution=(1920, 1080)))
+
+
